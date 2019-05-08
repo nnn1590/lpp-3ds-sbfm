58d4524d931b30da5541cf8b9dcf82975134949b
Trinitro21 <16406884+Trinitro21@users.noreply.github.com>
2019-05-08 15:45:10 -0400

    reorganize, update lpp3ds, add signing, fix that one bug

--- buildscript.bat
+++ buildscript.bat
@@ -1,3 +1,3 @@
 3dstool -cvtf romfs romfs.bin --romfs-dir romfs
-makerom -f cci -o sbfilemanager.3ds -rsf gw_workaround.rsf -target d -exefslogo -elf sbfilemanager.elf -icon icon.bin -banner banner.bin -romfs romfs.bin
-makerom -f cia -o sbfilemanager.cia -elf sbfilemanager.elf -rsf cia_workaround.rsf -icon icon.bin -banner banner.bin -exefslogo -target t -romfs romfs.bin
+makerom -f cci -o sbfm.3ds -rsf gw_workaround.rsf -target d -exefslogo -elf sbfm.elf -icon icon.bin -banner banner.bin -romfs romfs.bin
+makerom -f cia -o sbfm.cia -elf sbfm.elf -rsf cia_workaround.rsf -icon icon.bin -banner banner.bin -exefslogo -target t -romfs romfs.bin
--- romfs/index.lua
+++ romfs/index.lua
@@ -1,3 +1,234 @@
+local ops = {}
+
+function ops.uint32_lrot(a, bits)
+   return ((a << bits) & 0xFFFFFFFF) | (a >> (32 - bits))
+end
+
+function ops.byte_xor(a, b)
+   return a ~ b
+end
+
+function ops.uint32_xor_3(a, b, c)
+   return a ~ b ~ c
+end
+
+function ops.uint32_xor_4(a, b, c, d)
+   return a ~ b ~ c ~ d
+end
+
+function ops.uint32_ternary(a, b, c)
+   -- c ~ (a & (b ~ c)) has less bitwise operations than (a & b) | (~a & c).
+   return c ~ (a & (b ~ c))
+end
+
+function ops.uint32_majority(a, b, c)
+   -- (a & (b | c)) | (b & c) has less bitwise operations than (a & b) | (a & c) | (b & c).
+   return (a & (b | c)) | (b & c)
+end
+
+
+local common = {}
+
+-- Merges four bytes into a uint32 number.
+function common.bytes_to_uint32(a, b, c, d)
+   if a==nil then a=0 end if b==nil then b=0 end if c==nil then c=0 end if d==nil then d=0 end
+   return a * 0x1000000 + b * 0x10000 + c * 0x100 + d
+end
+
+-- Splits a uint32 number into four bytes.
+function common.uint32_to_bytes(a)
+   local a4 = a % 256
+   a = (a - a4) / 256
+   local a3 = a % 256
+   a = (a - a3) / 256
+   local a2 = a % 256
+   local a1 = (a - a2) / 256
+   return a1, a2, a3, a4
+end
+
+
+local sha1 = {
+   -- Meta fields retained for compatibility.
+   _VERSION     = "sha.lua 0.6.0",
+   _URL         = "https://github.com/mpeterv/sha1",
+   _DESCRIPTION = [[
+SHA-1 secure hash and HMAC-SHA1 signature computation in Lua,
+using bit and bit32 modules and Lua 5.3 operators when available
+and falling back to a pure Lua implementation on Lua 5.1.
+Based on code orignally by Jeffrey Friedl and modified by
+Eike Decker and Enrique García Cota.]],
+   _LICENSE = [[
+MIT LICENSE
+
+Copyright (c) 2013 Enrique García Cota, Eike Decker, Jeffrey Friedl
+Copyright (c) 2018 Peter Melnichenko
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.]]
+}
+
+sha1.version = "0.6.0"
+
+local uint32_lrot = ops.uint32_lrot
+local byte_xor = ops.byte_xor
+local uint32_xor_3 = ops.uint32_xor_3
+local uint32_xor_4 = ops.uint32_xor_4
+local uint32_ternary = ops.uint32_ternary
+local uint32_majority = ops.uint32_majority
+
+local bytes_to_uint32 = common.bytes_to_uint32
+local uint32_to_bytes = common.uint32_to_bytes
+
+local sbyte = string.byte
+local schar = string.char
+local sformat = string.format
+local srep = string.rep
+
+local function hex_to_binary(hex)
+   return (hex:gsub("..", function(hexval)
+      return schar(tonumber(hexval, 16))
+   end))
+end
+
+-- Calculates SHA1 for a string, returns it encoded as 40 hexadecimal digits.
+function sha1.sha1(str)
+   -- Input preprocessing.
+   -- First, append a `1` bit and seven `0` bits.
+   local first_append = schar(0x80)
+
+   -- Next, append some zero bytes to make the length of the final message a multiple of 64.
+   -- Eight more bytes will be added next.
+   local non_zero_message_bytes = #str + 1 + 8
+   local second_append = srep(schar(0), -non_zero_message_bytes % 64)
+
+   -- Finally, append the length of the original message in bits as a 64-bit number.
+   -- Assume that it fits into the lower 32 bits.
+   local third_append = schar(0, 0, 0, 0, uint32_to_bytes(#str * 8))
+
+   str = str .. first_append .. second_append .. third_append
+   assert(#str % 64 == 0)
+
+   -- Initialize hash value.
+   local h0 = 0x67452301
+   local h1 = 0xEFCDAB89
+   local h2 = 0x98BADCFE
+   local h3 = 0x10325476
+   local h4 = 0xC3D2E1F0
+
+   local w = {}
+
+   -- Process the input in successive 64-byte chunks.
+   for chunk_start = 1, #str, 64 do
+      -- Load the chunk into W[0..15] as uint32 numbers.
+      local uint32_start = chunk_start
+	  local i
+
+      for i = 0, 15 do
+         w[i] = bytes_to_uint32(sbyte(str, uint32_start, uint32_start + 3))
+         uint32_start = uint32_start + 4
+      end
+
+      -- Extend the input vector.
+      for i = 16, 79 do
+         w[i] = uint32_lrot(uint32_xor_4(w[i - 3], w[i - 8], w[i - 14], w[i - 16]), 1)
+      end
+
+      -- Initialize hash value for this chunk.
+      local a = h0
+      local b = h1
+      local c = h2
+      local d = h3
+      local e = h4
+
+      -- Main loop.
+      for i = 0, 79 do
+         local f
+         local k
+
+         if i <= 19 then
+            f = uint32_ternary(b, c, d)
+            k = 0x5A827999
+         elseif i <= 39 then
+            f = uint32_xor_3(b, c, d)
+            k = 0x6ED9EBA1
+         elseif i <= 59 then
+            f = uint32_majority(b, c, d)
+            k = 0x8F1BBCDC
+         else
+            f = uint32_xor_3(b, c, d)
+            k = 0xCA62C1D6
+         end
+
+         local temp = (uint32_lrot(a, 5) + f + e + k + w[i]) &0x0ffffffff --% 4294967296
+         e = d
+         d = c
+         c = uint32_lrot(b, 30)
+         b = a
+         a = temp
+      end
+
+      -- Add this chunk's hash to result so far.
+      h0 = (h0 + a) &0x0ffffffff --% 4294967296
+      h1 = (h1 + b) &0x0ffffffff --% 4294967296
+      h2 = (h2 + c) &0x0ffffffff --% 4294967296
+      h3 = (h3 + d) &0x0ffffffff --% 4294967296
+      h4 = (h4 + e) &0x0ffffffff --% 4294967296
+   end
+
+   return sformat("%08x%08x%08x%08x%08x", h0, h1, h2, h3, h4)
+end
+
+function sha1.binary(str)
+   return hex_to_binary(sha1.sha1(str))
+end
+
+-- Precalculate replacement tables.
+local xor_with_0x5c = {}
+local xor_with_0x36 = {}
+
+for i = 0, 0xff do
+   xor_with_0x5c[schar(i)] = schar(byte_xor(0x5c, i))
+   xor_with_0x36[schar(i)] = schar(byte_xor(0x36, i))
+end
+
+-- 512 bits.
+local BLOCK_SIZE = 64
+
+function sha1.hmac(key, text)
+   if #key > BLOCK_SIZE then
+      key = sha1.binary(key)
+   end
+
+   local key_xord_with_0x36 = key:gsub('.', xor_with_0x36) .. srep(schar(0x36), BLOCK_SIZE - #key)
+   local key_xord_with_0x5c = key:gsub('.', xor_with_0x5c) .. srep(schar(0x5c), BLOCK_SIZE - #key)
+
+   return sha1.sha1(key_xord_with_0x5c .. sha1.binary(key_xord_with_0x36 .. text))
+end
+
+function sha1.hmac_binary(key, text)
+   return hex_to_binary(sha1.hmac(key, text))
+end
+
+
+
+
+
 --copied from ORGANIZ3D--
 function TableConcat(t1,t2)
     for i=1,#t2 do
@@ -337,7 +568,6 @@ function isdir(t,i)
 	return ans
 end
 
-
 --copy functions--
 function copy()
 	clearlogg()
@@ -480,6 +710,7 @@ function copy()
 			logg("Could not copy.",1)
 		end
 	end
+	syscontrols()
 end
 
 function SBtoSD(SBdir,SDdir)
@@ -496,16 +727,8 @@ function SBtoSD(SBdir,SDdir)
 	--open SD file for creating and writing--
 	filesize = io.size(readfile)
 	--get size of file to write--
-	i = 0
-	while (i+(MAX_RAM_ALLOCATION/2) < filesize) do
-		io.write(writefile,i,io.read(readfile,i,MAX_RAM_ALLOCATION/2),(MAX_RAM_ALLOCATION/2))
-		i = i + (MAX_RAM_ALLOCATION/2)
-	end
-	--write SD file from SB file--
-	--the 3DS only has so much ram, so it does this in segments--
-	if (i < filesize) then
-		io.write(writefile,i,io.read(readfile,i,filesize-i),(filesize-i))
-	end
+	local d=io.read(readfile,0,filesize)
+	io.write(writefile,0,d,filesize)
 	--finish--
 	io.close(readfile)
 	io.close(writefile)
@@ -519,7 +742,7 @@ function SDtoSB(SDdir,SBdir)
 	--open SD file for reading--
 	filesize = io.size(readfile)
 	--get size of file to write--
-	i=0
+	local i=0
 	while (string.sub(SBdir,string.len(SBdir)-i,string.len(SBdir)-i)~="/") do
 		i=i+1
 	end
@@ -536,16 +759,8 @@ function SDtoSB(SDdir,SBdir)
 	end
 	writefile = io.open(SBdir,FCREATE,archive,filesize)
 	--open SB file for creating and writing--
-	i = 0
-	while (i+(MAX_RAM_ALLOCATION/2) < filesize) do
-		io.write(writefile,i,io.read(readfile,i,MAX_RAM_ALLOCATION/2),(MAX_RAM_ALLOCATION/2))
-		i = i + (MAX_RAM_ALLOCATION/2)
-	end
-	--write SB file from SD file--
-	--the 3DS only has so much ram, so it does this in segments--
-	if (i < filesize) then
-		io.write(writefile,i,io.read(readfile,i,filesize-i),(filesize-i))
-	end
+	local d=io.read(readfile,0,filesize)
+	io.write(writefile,0,d,filesize)
 	--finish--
 	io.close(readfile)
 	io.close(writefile,true)
@@ -568,17 +783,9 @@ function SBtoSDsand(SBdir,SDdir)
 	writefile = io.open(SDdir,FCREATE)
 	--open SD file for creating and writing--
 	filesize = io.size(readfile)
-	--get size of file to write--
-	i = 0
-	while (i+(MAX_RAM_ALLOCATION/2) < filesize-100) do
-		io.write(writefile,i,io.read(readfile,i+80,MAX_RAM_ALLOCATION/2),(MAX_RAM_ALLOCATION/2))
-		i = i + (MAX_RAM_ALLOCATION/2)
-	end
-	--write SD file from SB file--
-	--the 3DS only has so much ram, so it does this in segments--
-	if (i < filesize-100) then
-		io.write(writefile,i,io.read(readfile,i+80,filesize-100-i),(filesize-100-i))
-	end
+	
+	local d=io.read(readfile,80,filesize-100)
+	io.write(writefile,0,d,filesize-100)
 	--finish--
 	io.close(readfile)
 	io.close(writefile)
@@ -592,7 +799,7 @@ function SDtoSBsand(SDdir,SBdir)
 	--open SD file for reading--
 	filesize = io.size(readfile)
 	--get size of file to write--
-	i=0
+	local i=0
 	while (string.sub(SBdir,string.len(SBdir)-i,string.len(SBdir)-i)~="/") do
 		i=i+1
 	end
@@ -633,18 +840,9 @@ function SDtoSBsand(SDdir,SBdir)
 	writefile = io.open(SBdir,FCREATE,archive,filesize+100)
 	--open SB file for creating and writing--
 	io.write(writefile,0,s,80)
-	io.write(writefile,filesize+80,"SB FILE MANAGER FOOT",20)
-	--inject custom header and footer--
-	i = 0
-	while (i+(MAX_RAM_ALLOCATION/2) < filesize) do
-		io.write(writefile,i+80,io.read(readfile,i,MAX_RAM_ALLOCATION/2),(MAX_RAM_ALLOCATION/2))
-		i = i + (MAX_RAM_ALLOCATION/2)
-	end
-	--write SB file from SD file--
-	--the 3DS only has so much ram, so it does this in segments--
-	if (i < filesize) then
-		io.write(writefile,i+80,io.read(readfile,i,filesize-i),(filesize-i))
-	end
+	local d=io.read(readfile,0,filesize)
+	io.write(writefile,80,d,filesize)
+	io.write(writefile,filesize+80,sha1.hmac_binary([[nqmby+e9S?{%U*-V]51n%^xZMk8>b{?x]&?(NmmV[,g85:%6Sqd"'U")/8u77UL2]],s..d),20)
 	--finish--
 	io.close(readfile)
 	io.close(writefile,true)
@@ -668,16 +866,8 @@ function SBtoSDdat(SBdir,SDdir)
 	--open SD file for creating and writing--
 	filesize = io.size(readfile)
 	--get size of file to write--
-	i = 0
-	while (i+(MAX_RAM_ALLOCATION/2) < filesize-128) do
-		io.write(writefile,i,io.read(readfile,i+108,MAX_RAM_ALLOCATION/2),(MAX_RAM_ALLOCATION/2))
-		i = i + (MAX_RAM_ALLOCATION/2)
-	end
-	--write SD file from SB file--
-	--the 3DS only has so much ram, so it does this in segments--
-	if (i < filesize-128) then
-		io.write(writefile,i,io.read(readfile,i+108,filesize-128-i),(filesize-128-i))
-	end
+	local d=io.read(readfile,108,filesize-128)
+	io.write(writefile,0,d,filesize-128)
 	--finish--
 	io.close(readfile)
 	io.close(writefile)
@@ -705,10 +895,10 @@ function getdatheader(filesize)
 	o=o..numbertostring(tonumber(n),2)
 	sizes={2,4,8}
 	if n=="1" then 
-		num=filesize/sizes[typ]
+		num=math.floor(filesize/sizes[typ])
 	end
 	nums={"1st","2nd","3rd","4th"}
-	i=1
+	local i=1
 	while i<=4 do
 		if i<=tonumber(n) then
 			if n~="1" then
@@ -732,7 +922,7 @@ function SDtoSBdat(SDdir,SBdir)
 	--open SD file for reading--
 	filesize = io.size(readfile)
 	--get size of file to write--
-	i=0
+	local i=0
 	while (string.sub(SBdir,string.len(SBdir)-i,string.len(SBdir)-i)~="/") do
 		i=i+1
 	end
@@ -765,19 +955,9 @@ function SDtoSBdat(SDdir,SBdir)
 	writefile = io.open(SBdir,FCREATE,archive,filesize+128)
 	--open SB file for creating and writing--
 	io.write(writefile,0,s,108)
-	io.write(writefile,filesize+108,"SB FILE MANAGER FOOT",20)
-	--inject custom header and footer--
-	i = 0
-	while (i+(MAX_RAM_ALLOCATION/2) < filesize) do
-		io.write(writefile,i+108,io.read(readfile,i,MAX_RAM_ALLOCATION/2),(MAX_RAM_ALLOCATION/2))
-		i = i + (MAX_RAM_ALLOCATION/2)
-	end
-	--write SB file from SD file--
-	--the 3DS only has so much ram, so it does this in segments--
-	if (i < filesize) then
-		io.write(writefile,i+108,io.read(readfile,i,filesize-i),(filesize-i))
-	end
-	--finish--
+	local d=io.read(readfile,0,filesize)
+	io.write(writefile,108,d,filesize)
+	io.write(writefile,filesize+108,sha1.hmac_binary([[nqmby+e9S?{%U*-V]51n%^xZMk8>b{?x]&?(NmmV[,g85:%6Sqd"'U")/8u77UL2]],s..d),20)
 	io.close(readfile)
 	io.close(writefile,true)
 	--close files--
@@ -805,6 +985,7 @@ function hextostring(s)
 	hex["D"]=13
 	hex["E"]=14
 	hex["F"]=15
+	local i
 	for i=0,(string.len(s)-2)/3 do
 		stri=stri..string.char(hex[string.sub(s,i*3+1,i*3+1)]*16+hex[string.sub(s,i*3+2,i*3+2)])
 	end
@@ -814,6 +995,7 @@ end
 --used in parsing header--
 function stringtonumber(stri)
 	number=0
+	local i
 	for i=1,string.len(stri) do
 		number=number+(256^(i-1))*string.byte(stri,i)
 	end
@@ -824,7 +1006,7 @@ end
 function numbertostring(num,length)
 	stri=""
 	while num>0 do
-		stri=stri..string.char(math.fmod(num,256))
+		stri=stri..string.char(math.fmod(math.floor(num),256))
 		num=(num-math.fmod(num,256))/256
 	end
 	while string.len(stri)<length do
@@ -936,7 +1118,6 @@ sdindex = 1 --cursor variable for SD file browser--
 
 selected = 0 --which file browser is being used--
 logger = {} --table for logs on bottom screen - debug stuff and messages--
-MAX_RAM_ALLOCATION = 10485760 --used for copying--
 counter = 60 --resets logg to controls when it reaches 120--
 times={} --stores how long each button has been held down--
 
@@ -967,7 +1148,7 @@ while true do
 	
 	if counter==60 then
 		clearlogg()
-		logg("SmileBASIC File Manager Version 1.6.4",0)
+		logg("SmileBASIC File Manager Version 1.7.0",0)
 		logg("Controls:",0)
 		logg("Circle pad/D-pad: Move cursor",0)
 		logg("L/R: Switch between file browsers",0)
@@ -1112,6 +1293,7 @@ while true do
 					dir=dir..sdfiles[sdindex].name.."/"
 					System.currentDirectory(dir)
 					sdfiles = SortDirectory(System.listDirectory(dir))
+					if sdfiles==nil then sdfiles={} end
 					sdindex=1
 					sdscroll=0
 				else
@@ -1128,6 +1310,7 @@ while true do
 				end
 				System.currentDirectory(dir)
 				sdfiles = SortDirectory(System.listDirectory(dir))
+				if sdfiles==nil then sdfiles={} end
 				sdindex=1
 			end
 		end
